d129d1cd3beed514b7eb60df8bc8a5fb7b86f498
Bharat Dev Burman <bharat.singh7924@gmail.com>
2024-08-07 16:45:33 +0530

    some nice changes
    1. run npm audit fix --force
    2. remove unused dependencies on firebase
    3. remove unused import statements
    4. use correct color value for Typography
    5. position input box with sticky
    6. set input box width according to viewport width
    7. Split the username and message with left align
    
    Signed-off-by: Bharat Dev Burman <bharat.singh7924@gmail.com>

--- messenger-mern-starter-project/src/App.js
+++ messenger-mern-starter-project/src/App.js
@@ -1,102 +1,94 @@
-import React, { useEffect, useState } from 'react';
-import './App.css';
-import { Button, FormControl, InputLabel, Input } from '@material-ui/core'
-import Message from './Message';
+import React, { useEffect, useState } from "react";
+import "./App.css";
+import { FormControl, Input } from "@material-ui/core";
+import Message from "./Message";
 
-import FlipMove from 'react-flip-move'
-import SendIcon from '@material-ui/icons/Send'
-import IconButton from '@material-ui/core/IconButton'
-import io from 'socket.io-client'
-import axios from './axios';
-
-
+import FlipMove from "react-flip-move";
+import SendIcon from "@material-ui/icons/Send";
+import IconButton from "@material-ui/core/IconButton";
+import io from "socket.io-client";
+import axios from "./axios";
 
 const socket = io.connect("http://localhost:3001");
 
-function App()
-{
-  const [input, setInput] = useState('')
-  const [messages, setMessages] = useState([])
-  const [username, setUsername] = useState('')
+function App() {
+  const [input, setInput] = useState("");
+  const [messages, setMessages] = useState([]);
+  const [username, setUsername] = useState("");
 
-  
-  
-
-  
-  useEffect(() =>
-  {
-    const sync = async () =>
-    {
-      console.log("axios")
-      await axios.get('/retreieve/conversation').then((res =>
-      {
+  useEffect(() => {
+    const sync = async () => {
+      console.log("axios");
+      await axios.get("/retreieve/conversation").then((res) => {
         setMessages(res.data);
-        console.log(res)
-      }))
-    }
-    sync()
-  }, [input])
+        // console.log(res);
+      });
+    };
+    sync();
+  }, [input]);
 
+  useEffect(() => {
+    setUsername(prompt("Please enter your name"));
+  }, []);
 
+  useEffect(() => {
+    console.log("this is receiving");
+    socket.on("receive_message", (data) => {
+      console.log("Received", data);
+      setMessages((prevMessages) => [...prevMessages, data]);
+    });
+  }, []);
 
-  useEffect(() =>
-  {
-    setUsername(prompt('Please enter your name'))
-  }, [])
-
-
-  useEffect(() =>
-  {
-    console.log("this is receiving")
-    socket.on("receive_message", (data) =>
-    {
-      console.log("Received", data)
-      setMessages(prevMessages => [...prevMessages, data]);
-      
-
-    })
-  }, [socket])
-
-
-  const sendMessage = async (e) =>
-  {
+  const sendMessage = async (e) => {
     e.preventDefault();
 
     const message = {
       username: username,
       message: input,
-      timestamp: Date.now()
+      timestamp: Date.now(),
     };
 
     socket.emit("send_message", message);
 
-    await axios.post('/save/message', message);
+    await axios.post("/save/message", message);
 
-    setInput('');
+    setInput("");
   };
-
 
   return (
     <div className="App">
-      <img src="https://play.google.com/store/apps/details?id=world.mnetplus.talk.aos&hl=en_US" alt="messenger logo" />
+      <img
+        src="https://play-lh.googleusercontent.com/8xSX7C0edYgitTGwdbqgoB9jt5lp4nO7VY_jkE3jWUCypcugn5aRHSn9Y8-qQA53GSo=w240-h480-rw"
+        alt="messenger logo"
+      />
       <h2>Welcome {username}</h2>
 
-      <form className='app__form' >
-        <FormControl className='app__formControl' >
-          <Input className='app__input' placeholder='Enter a message...' value={input} onChange={(e) => setInput(e.target.value)} />
-          <IconButton className='app__iconButton' variant='text' color='primary' disabled={!input} onClick={sendMessage} type="submit" >
+      <FlipMove>
+        {messages.map((message) => (
+          <Message key={message._id} message={message} username={username} />
+        ))}
+      </FlipMove>
+
+      <form className="app__form">
+        <FormControl className="app__formControl">
+          <Input
+            className="app__input"
+            placeholder="Enter a message..."
+            value={input}
+            onChange={(e) => setInput(e.target.value)}
+          />
+          <IconButton
+            className="app__iconButton"
+            variant="text"
+            color="primary"
+            disabled={!input}
+            onClick={sendMessage}
+            type="submit"
+          >
             <SendIcon />
           </IconButton>
         </FormControl>
       </form>
-
-      <FlipMove>
-        {
-          messages.map(message => (
-            <Message key={message._id} message={message} username={username} />
-          ))
-        }
-      </FlipMove>
     </div>
   );
 }
