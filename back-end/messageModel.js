6ae2a406c2a982f6a3f048f7712ff66612e9d26a
Anand <jeevaanand93002@gmail.com>
2024-08-08 13:10:51 +0530

    online status is done

--- back-end/messageModel.js
+++ back-end/messageModel.js
@@ -3,7 +3,10 @@ const mongoose = require("mongoose")
 const messageSchema = mongoose.Schema({
     username: String,
     message: String,    
-    timestamp: String
+    timestamp: String,
+    delivered: Boolean,
+    edited: Boolean,
+    online: Boolean
 });
 
 const typeModel = mongoose.model('messages', messageSchema)
